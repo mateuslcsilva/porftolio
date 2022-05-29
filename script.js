5ca824a271e14b8da358b2ed0748276ecfb104ca
Mateus Leite <mateus.lcsilva@outlook.com>
2022-05-29 11:07:56 -0300

    finally ready

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+let sectionActive = 0
+
+
 const closeFloaterDiv = () => {
     let floaterDivs = document.querySelectorAll('.floaterDiv')
-    
+
     floaterDivs.forEach((element) => {
         element.classList.remove('floaterDivActive')
     })
 
+    resetProjectView(), 1000
 }
 
 const floaterDivActive = (index) => {
@@ -14,5 +18,54 @@ const floaterDivActive = (index) => {
         element.classList.remove('floaterDivActive')
     })
 
+    resetProjectView()
     floaterDivs[index].classList.add('floaterDivActive')
+}
+
+const nextProjectButton = () => {
+    switch (sectionActive) {
+        case 0: {
+            document.querySelector('.carroselDivTwo').classList.add('carroselDivActive')
+            document.querySelector('.carroselDivOne').classList.remove('carroselDivActive')
+            document.querySelector('.arrowPreviousProject').style.display = 'block'
+            sectionActive++
+            break
+        }
+        case 1: {
+            document.querySelector('.carroselDivThree').classList.add('carroselDivActive')
+            document.querySelector('.carroselDivTwo').classList.remove('carroselDivActive')
+            document.querySelector('.arrowNextProject').style.display = 'none'
+            sectionActive++
+            break
+        }
+    }
+}
+
+
+const previousProjectButton = () => {
+    switch (sectionActive) {
+        case 1: {
+            document.querySelector('.carroselDivTwo').classList.remove('carroselDivActive')
+            document.querySelector('.carroselDivOne').classList.add('carroselDivActive')
+            document.querySelector('.arrowPreviousProject').style.display = 'none'
+            sectionActive--
+            break
+        }
+        case 2: {
+            document.querySelector('.carroselDivTwo').classList.add('carroselDivActive')
+            document.querySelector('.carroselDivThree').classList.remove('carroselDivActive')
+            document.querySelector('.arrowNextProject').style.display = 'block'
+            sectionActive--
+            break
+        }
+    }
+}
+
+const resetProjectView = () => {
+    document.querySelector('.carroselDivTwo').classList.remove('carroselDivActive')
+    document.querySelector('.carroselDivThree').classList.remove('carroselDivActive')
+    document.querySelector('.carroselDivOne').classList.add('carroselDivActive')
+    document.querySelector('.arrowNextProject').style.display = 'block'
+    document.querySelector('.arrowPreviousProject').style.display = 'none'
+    sectionActive = 0
 }
